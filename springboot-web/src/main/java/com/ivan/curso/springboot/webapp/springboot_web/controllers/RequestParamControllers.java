547655529708b117ce84a0079eb00c9874fe71e8
Ivan Gallego Marquez <ivan17marquez@gmail.com>
2026-01-15 14:04:08 +0100

    Add endpoint to handle request parameters via HttpServletRequest
    Introduced a new GET endpoint '/request' in RequestParamControllers that uses HttpServletRequest to extract 'code' and 'message' parameters, populating a ParamMixDto object. This provides an alternative way to handle request parameters beyond @RequestParam.

--- springboot-web/src/main/java/com/ivan/curso/springboot/webapp/springboot_web/controllers/RequestParamControllers.java
+++ springboot-web/src/main/java/com/ivan/curso/springboot/webapp/springboot_web/controllers/RequestParamControllers.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ivan.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 import com.ivan.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +37,19 @@ public class RequestParamControllers {
             return params;
     }
         
-
+    @GetMapping("/request")
+            public ParamMixDto request(HttpServletRequest request){
+                Integer code = 10;
+                try {
+                code = Integer.parseInt (request.getParameter("code"));
+                } catch (NumberFormatException e) {             }  
+                
+                
+                ParamMixDto params = new ParamMixDto();
+                params.setCode(code);
+                params.setMessage(request.getParameter("message"));
+                return params;
+            }
+    
+    
 }
