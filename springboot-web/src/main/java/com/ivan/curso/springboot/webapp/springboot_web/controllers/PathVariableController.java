818e78bd99521f9a4436573ad05f57d3c18f2162
Ivan Gallego Marquez <ivan17marquez@gmail.com>
2026-01-20 13:26:22 +0100

    Add external config and new endpoints to controller
    Introduced ValuesConfig to load values.properties, added values.properties file with custom config, and updated PathVariableController to inject and expose these properties via a new /values endpoint. Also added a POST /create endpoint for User creation and made minor cleanup to application.properties.

--- springboot-web/src/main/java/com/ivan/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/ivan/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,19 +3,40 @@ package com.ivan.curso.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ivan.curso.springboot.webapp.springboot_web.models.User;
 import com.ivan.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
 @RestController
 @RequestMapping("api/var")
 public class PathVariableController {
+
+@Value("${config.username}")
+private String userName;
+
+@Value("${config.code}")
+private String code;   
+
+// @Value("${config.message}")
+// private String message;
+
+@Value("${config.listOfValues}")
+private String[] listOfValues;
+
+
+
     
 @GetMapping("/baz/{message}")
 public ParamDto baz(@PathVariable String message) {
@@ -35,6 +56,32 @@ public Map<String, Object>PathVar(@PathVariable String product, @PathVariable Lo
     return json;
 // ejemplo de ruta para ver que funciona correctamente en el navegador : http://localhost:8080/api/var/mix/cocacola/1234
 }
+
+@PostMapping("/create")
+public User create(@RequestBody User user){
+    
+
+    user.setName(user.getName().toUpperCase());
+    //Hacer algo con el usaurio (como un saver) en la BBDD
+    return user;
 }
+
+@GetMapping("/values")
+    public Map<String, Object> Values(@Value("${config.message}") String message){
+
+    Map<String, Object> json = new HashMap<>();
+    json.put("username", userName);
+    json.put("code", code);
+    json.put("message", message);
+    json.put("listOfValues", listOfValues);
+
+    return json;
+}
+
+
+
+
+}
+
 
 
